25b915881f289e0c377aef981e6e486c33e2f691
Umar <umar.317@gmail.com>
2024-10-08 20:24:53 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Presale from "./pages/Presale";
 
 function App() {
   return (
-    <div className="font-poppins text-white bg-[url('./bg.png')] bg-cover bg-center h-screen">
+    <div className="font-poppins text-white bg-[url('/bg.png')] bg-cover bg-center h-screen">
       <div className="bg-yellow-300/2 backdrop-blur-sm h-screen flex justify-center items-center">
         <Presale />
       </div>
